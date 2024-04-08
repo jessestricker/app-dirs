09327874cab86d040f4c677dfbb245b201f3910c
Jesse Stricker <git@jessestricker.de>
2024-04-08 21:14:36 +0200

    build: setup Maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     alias(libs.plugins.kotlin)
     `java-library`
+    `maven-publish`
 
     alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
 }
+
+group = "de.jessestricker"
+version = "0.1.0-pre-0"
 
 repositories {
     mavenCentral()
@@ -18,6 +22,28 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/jessestricker/app-dirs")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 detekt {
